6572972f90f3247e2062ce3095261ad2897cb28c
alifurkan <alifrknak@gmail.com>
2023-12-13 14:15:14 +0300

    12/13/2023

--- Web/Controllers/BlogController.cs
+++ Web/Controllers/BlogController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Web.Models;
 
 namespace Web.Controllers
 {
 	public class BlogController : Controller
 	{
+		Context _context = new Context();
 		public IActionResult Index()
 		{
-			return View();
+			var blogs =_context.Blogs.ToList();
+
+			return View(blogs);
+		}
+		public IActionResult BlogDetail(int blogId)
+		{
+			var blogs = _context.Blogs.Where(b => b.Id == blogId);
+
+			return View(blogs);
 		}
 	}
 }
