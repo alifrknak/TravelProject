6eda8cb68d5787ba1143bbecb726f5929ba03137
alifurkan <alifrknak@gmail.com>
2023-12-16 21:05:54 +0300

    12/16/2022

--- Web/Controllers/BlogController.cs
+++ Web/Controllers/BlogController.cs
@@ -8,13 +8,16 @@ namespace Web.Controllers
 		Context _context = new Context();
 		public IActionResult Index()
 		{
-			var blogs =_context.Blogs.ToList();
+			var blogs = _context.Blogs.ToList();
+
+			// The newest post has the smallest date.
+			ViewData["recentPosts"] = _context.Blogs.Take(3).OrderBy(q => q.CreateTime);
 
 			return View(blogs);
 		}
 		public IActionResult BlogDetail(int blogId)
 		{
-			var blogs = _context.Blogs.Where(b => b.Id == blogId);
+			var blogs = _context.Blogs.Single(b => b.Id == blogId);
 
 			return View(blogs);
 		}
--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Web.Models;
 
 namespace Web.Controllers
 {
 	public class HomeController : Controller
 	{
+		Context _context = new Context();
+
 		public IActionResult Index()
 		{
-			return View();
+			var blogs = _context.Blogs.ToList();
+
+			return View(blogs);
+		}
+		public PartialViewResult GetPartialView()
+		{
+			var values = _context.Blogs.OrderByDescending(q => q.Id).Take(2).ToList();
+			return PartialView(values);
 		}
         
     }
